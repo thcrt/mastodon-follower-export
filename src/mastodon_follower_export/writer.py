a8a2cfaedcfee5cca8f1df682746133a2760d950
thcrt <110127860+thcrt@users.noreply.github.com>
2025-09-20 02:29:41 +0200

    Always write UTF-8

--- src/mastodon_follower_export/writer.py
+++ src/mastodon_follower_export/writer.py
@@ -6,7 +6,7 @@ from .wrapper import Follower
 
 
 def write(followers: list[Follower], path: Path) -> None:
-    with path.open("w+", newline="") as f:
+    with path.open("w+", newline="", encoding="utf-8") as f:
         writer = csv.DictWriter(
             f,
             quoting=csv.QUOTE_NOTNULL,
