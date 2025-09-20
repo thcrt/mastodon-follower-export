9ff9d8ee42e5ab88d44230a7a80ecb101dfb2e6f
thcrt <110127860+thcrt@users.noreply.github.com>
2025-09-20 18:47:34 +0200

    Use default OS style, change to `ModernStyle` for wizard
    This negates [QTBUG-123853](https://bugreports.qt.io/browse/QTBUG-123853).

--- src/mastodon_follower_export/__main__.py
+++ src/mastodon_follower_export/__main__.py
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QWizard
 
 from .wizard import MainWizard
 
 if __name__ == "__main__":
-    app = QApplication([])
-    app.setStyle("Fusion")
+    app = QApplication()
     wizard = MainWizard()
+    wizard.setWizardStyle(QWizard.WizardStyle.ModernStyle)
     wizard.show()
     app.exec()
